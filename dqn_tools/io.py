d308dcd2506510d1e56fd44fab839b61a580477a
Grzegorz Jaworski <g.jaworski1996@gmail.com>
2019-08-15 21:17:27 +0200

    Corrected training and prepared new model

--- dqn_tools/io.py
+++ dqn_tools/io.py
@@ -4,18 +4,21 @@ import keras
 from keras.engine.saving import load_model
 
 
-def save(directory: str, name: str, active_model: keras.models.Model, target_model: keras.models.Model, memory):
+def save(directory: str, name: str, active_model: keras.models.Model, target_model: keras.models.Model, memory=None):
     if not os.path.isdir(directory):
         os.mkdir(directory)
     active_model.save("{}/{}_active.h5f".format(directory, name))
     target_model.save("{}/{}_target.h5f".format(directory, name))
-    with open("{}/{}_memory.obj".format(directory, name), 'wb') as handler:
-        pickle.dump(memory, handler, pickle.HIGHEST_PROTOCOL)
+    if memory is not None:
+        with open("{}/{}_memory.obj".format(directory, name), 'wb') as handler:
+            pickle.dump(memory, handler, pickle.HIGHEST_PROTOCOL)
 
 
-def load(directory: str, name: str):
+def load(directory: str, name: str, has_memory: bool=False):
     active_model = load_model("{}/{}_active.h5f".format(directory, name))
     target_model = load_model("{}/{}_target.h5f".format(directory, name))
-    with open("{}/{}_memory.obj".format(directory, name), 'rb') as handler:
-        memory = pickle.load(handler)
+    memory = None
+    if has_memory:
+        with open("{}/{}_memory.obj".format(directory, name), 'rb') as handler:
+            memory = pickle.load(handler)
     return active_model, target_model, memory
--- train_model.py
+++ train_model.py
@@ -1,145 +1,32 @@
 import numpy as np
-import keras
-from keras.initializers import RandomNormal
-from keras.layers import Dense, LeakyReLU
-from keras.regularizers import l2
 
 import chess_environment.chessboard as cb
 from dqn_tools.memory import SimpleMemory
 from dqn_tools.trainers import DQNTrainer, load_trainer
-from training_tools import DQNChessRecord
+from models.BuzdyganDQNv0.template import BuzdyganDQNv0Templte
 
 seed = 12345
 np.random.seed(seed)
 # temporary simple model for testing base concept
-weight_decay = l2(1e-2)
-weight_initializer = RandomNormal(mean=0., stddev=0.02, seed=seed)
-model = keras.Sequential([
-    Dense(150, input_shape=(384,),
-          kernel_initializer=weight_initializer,
-          bias_initializer=weight_initializer,
-          kernel_regularizer=weight_decay,
-          bias_regularizer=weight_decay),
-    LeakyReLU(alpha=0.3),
-    Dense(300,
-          kernel_initializer=weight_initializer,
-          bias_initializer=weight_initializer,
-          kernel_regularizer=weight_decay,
-          bias_regularizer=weight_decay),
-    LeakyReLU(alpha=0.3),
-    Dense(1, activation="linear",
-          kernel_initializer=weight_initializer,
-          bias_initializer=weight_initializer,
-          kernel_regularizer=weight_decay,
-          bias_regularizer=weight_decay)
-])
-model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-
-NAME = "LeakyDQNv1"
+model_template = BuzdyganDQNv0Templte()
 LOAD = False
-LOAD_FROM = "./tmp"
-START_AT_STEP = 0
-TRAINING_STEPS = int(2e+7)
-MEMORY_SIZE = int(1e+5)
-START_TRAINING_AT = int(2e+4)
-BATCH = 64
-GAMMA = 0.99
-THETA = 0.001
-EPSILON = 0.25
-EPSILON_TRESHOLD = START_TRAINING_AT * 1.01
-
-
-def get_epsilon(step: int):
-    return 1 if step < EPSILON_TRESHOLD else EPSILON
-
-
-def choose_action(model: keras.Model, possible_moves, possible_states, fens):
-    highest_prize = 0
-    best_move = None
-    best_state = None
-    best_state_fen = None
-    for m, s, f in zip(possible_moves, possible_states, fens):
-        prize = model.predict(np.array(s).reshape((1, 384)))
-        if prize > highest_prize or best_move is None:
-            highest_prize = prize
-            best_move = m
-            best_state = s
-            best_state_fen = f
-
-    return best_move, best_state, best_state_fen
-
-
-def action(acting_model: keras.Model, models_memory: SimpleMemory, environment: cb.ChessBoard, epsilon):
-    flip = not environment.current_turn()
-    moves, states, fens = environment.get_moves(flip=flip)
-    best_move = None
-    best_state = None
-    best_state_fen = None
-    if np.random.uniform(0, 1) < epsilon:
-        moves_states_list = list(map(list, zip(moves, states, fens)))
-        choices = len(moves_states_list)
-        if choices > 1:
-            random_element = moves_states_list[np.random.randint(0, choices)]
-        else:
-            random_element = moves_states_list[0]
-        best_move, best_state, best_state_fen = random_element
-    else:
-        best_move, best_state, best_state_fen = choose_action(acting_model, moves, states, fens)
-    # make move
-    environment.make_move(best_move, flip)
-    real_prize = environment.get_results()
-    best_state = np.array(best_state).reshape((384,))
-    real_prize = np.array([real_prize]).reshape((1, 1))
-    if real_prize == cb.IGNORE_GO:
-        return
-    record = DQNChessRecord()
-    record.state = best_state
-    record.fen = best_state_fen
-    record.reward = real_prize
-    models_memory.add(record)
-
-
-def training(
-        acting_model: keras.Model,
-        target_model: keras.Model,
-        models_memory: SimpleMemory,
-        batch_size: int,
-        gamma: float):
-    training_batch = models_memory.get_batch(batch_size, min_rows=START_TRAINING_AT)
-    if training_batch is not None:
-        samples = [[record.state, record.reward, record.fen] for record in training_batch]
-        states, prizes, fens = list(map(list, zip(*samples)))
-        reinforced_prizes = []
-        for p, f in zip(prizes, fens):
-            training_board = cb.ChessBoard(starting_fen=f)
-            p = p[0]
-            if not training_board.game_over():
-                next_moves, next_states, next_fens = training_board.get_moves()
-                _, chosen_state, _ = choose_action(acting_model, next_moves, np.array(next_states), next_fens)
-                estimated_next_prize = target_model.predict(np.array(chosen_state.reshape((1, 384))))[0]
-                reinforced_p = p + gamma * estimated_next_prize
-            else:
-                reinforced_p = p
-            reinforced_prizes.append(reinforced_p)
-
-        states = np.array(states)
-        reinforced_prizes = np.array(reinforced_prizes)
-        acting_model.train_on_batch(states, reinforced_prizes)
-
+LOAD_FROM = ""
 
 if LOAD:
-    model_trainer = load_trainer(LOAD_FROM, NAME, action, training)
+    model_trainer = load_trainer(LOAD_FROM, model_template.NAME, model_template.action, model_template.training)
 else:
-    memory = SimpleMemory(MEMORY_SIZE)
-    model_trainer = DQNTrainer(model, memory, action, training)
+    model = model_template.new_model(seed)
+    memory = SimpleMemory(model_template.MEMORY_SIZE)
+    model_trainer = DQNTrainer(model, memory, model_template.action, model_template.training)
 
 board = cb.ChessBoard()
-for i in range(START_AT_STEP, TRAINING_STEPS):
-    print("Step {} of {}".format(i+1, TRAINING_STEPS))
-    model_trainer.take_action(board, get_epsilon(i))
-    model_trainer.train(batch_size=BATCH, gamma=GAMMA, theta=THETA)
-    if i % 50000 == 0:
-        model_trainer.save("tmp", "{}_{}".format(NAME, i))
+for i in range(model_template.START_AT_STEP, model_template.TRAINING_STEPS):
+    print("Step {} of {}".format(i+1, model_template.TRAINING_STEPS))
+    model_trainer.take_action(board, model_template.get_epsilon(i))
+    model_trainer.train(batch_size=model_template.BATCH, gamma=model_template.GAMMA, theta=model_template.THETA)
+    if i % model_template.SAVE_PER_STEPS == 0:
+        model_trainer.save("tmp", "{}_{}".format(model_template.NAME, i))
 
-model_trainer.save("final", "{}_{}k".format(NAME, int(TRAINING_STEPS / 1000)))
+model_trainer.save("final", "{}_{}k".format(model_template.NAME,
+                                            int(model_template.TRAINING_STEPS / 1000)))
 
